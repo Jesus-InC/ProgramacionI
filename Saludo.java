8a031c7ac30487cd9363cdc4f49eb9706e7e29cd
Jesús Ibarra <125235437+Jesus-InC@users.noreply.github.com>
2023-08-22 03:26:45 -0400

    Update Saludo.java

---
+++ Saludo.java
@@ -0,0 +1,8 @@
+public class Saludo {
+    public static void main(String[] args) {
+        System.out.println("Por favor, escriba su nombre");
+        String nombre = System.console().readLine();
+
+        System.out.println("Bienvenido, " + nombre + ", es gusto saludarte.");
+    }
+}
